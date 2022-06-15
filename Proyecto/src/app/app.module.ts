14cfa68cbaf8674f2ffe9f690e92ebb326fae485
JuanjoArce <juanjosearceherrera@gmail.com>
2022-06-15 20:30:42 -0300

    create proyectos

--- Proyecto/src/app/app.module.ts
+++ Proyecto/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { FotologComponent } from './components/fotolog/fotolog.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { HComponent } from './components/h/h.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     FotologComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    ProyectosComponent,
+    HComponent
   ],
   imports: [
     BrowserModule,
